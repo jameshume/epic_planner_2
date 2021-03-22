fe6a2d0e6bdedeb482dfb4e2944fdf60263ded21
james.hume@utonomy.co.uk <james.hume@utonomy.co.uk>
2021-03-22 07:57:03 +0000

    Some progress playing with Cognito and the Amplify JS SDK

--- src/App.js
+++ src/App.js
@@ -10,31 +10,27 @@ Amplify.configure({
     Auth: {
         region: 'us-east-1',
         userPoolId: 'us-east-1_79qI7j2wz',
-        userPoolWebClientId: '6knjms22fvbb3karqbk2ijbast',
+        userPoolWebClientId: '332d6s8mq87na571o9aebi7ri',
         mandatorySignIn: true,
-        cookieStorage: {
-            domain: 'jehtech.com',
-            path: '/',
-            expires: 30,
-            sameSite: "strict",
-            secure: true
-        },
+        // TODO - Adding cookie storage stops the withAuthenticator displaying the app because
+        //        presumably it searches for a user in the cookie storage if its available but
+        //        doesn't find one. Maybe without it, it uses localStorage??
+        //cookieStorage: {
+        //    domain: '.jehtech.com',
+        //    path: '/',
+        //    expires: 30,
+        //    sameSite: "strict",
+        //    secure: true
+        //},
         authenticationFlowType: 'USER_PASSWORD_AUTH',
-        oauth: { //  Hosted UI configuration
-            domain: 'your_cognito_domain',
-            scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
-            redirectSignIn: 'http://localhost:3000/',
-            redirectSignOut: 'http://localhost:3000/',
-            responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
-        }
     }
 });
 
 
-function App() {
+const App = () => {
     return (
         <div className={Classes.App}>
-            <AmplifySignOut />
+            
             <header>
                 <div style={{width: "100%", height: "100%", "grid-template-columns": "1fr 1fr 1fr", display: "grid"}}>
                     <div style={{textAlign:"left",  margin: "auto 0 auto 1rem"}}>
@@ -45,8 +41,10 @@ function App() {
                     <div style={{textAlign:"center",  margin: "auto 0 auto 1rem"}}>
                         JEHTech EpicPlanner
                     </div>
-                    <div style={{textAlign:"right",  margin: "auto 1rem auto 0"}}>
-                        <button>Sign in</button>
+                    <div style={{textAlign:"right",  width: "10px", margin: "auto 1rem auto 0"}}>
+                        <div>
+                        <AmplifySignOut></AmplifySignOut>
+                        </div>
                     </div>
                 </div>
             </header>
